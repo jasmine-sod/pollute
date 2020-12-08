a17f56ca59bf1d81aef8c4f9a7dc077f87b54857
jasminesodhi <contact@jasminesodhi.com>
2020-12-09 03:25:04 +0530

    Login authentication

--- app/src/main/java/com/jasminesodhi/randomhacksofkindness/MainActivity.java
+++ app/src/main/java/com/jasminesodhi/randomhacksofkindness/MainActivity.java
@@ -15,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userLogin = (FancyButton) findViewById(R.id.user_login);
+        uberLogin = (FancyButton) findViewById(R.id.uber_login);
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                intent.putExtra("Type", "car");
+                startActivity(intent);
+            }
+        });
+
+        uberLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                intent.putExtra("Type", "uber");
+                startActivity(intent);
+            }
+        });
     }
 }
